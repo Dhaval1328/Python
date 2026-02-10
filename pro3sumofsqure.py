4741d657ef738b99de8961c2e3015332d7aeadb7
Dhaval Parmar <ddhaval2655@gmail.com>
2026-02-10 11:43:29 +0530

    Add files via upload

---
+++ pro3sumofsqure.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+
+sum = 0
+
+for i in range(1, n + 1):
+    sum = sum + i * i
+
+print("Sum of squares is:", sum)
---
+++ pro4factorialgivennumber.py
@@ -0,0 +1,10 @@
+"""w a p to print the factorial of given number"""
+
+n = int(input("Enter a number: "))
+
+fact = 1
+
+for i in range(1, n + 1):
+    fact = fact * i
+
+print("Factorial of", n, "is:", fact)
