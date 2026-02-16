da3b10b4bf467863da0cf6135ddcddfdf08dbbe3
Dhaval Parmar <ddhaval2655@gmail.com>
2026-02-16 14:58:08 +0530

    Add files via upload

---
+++ LeapYear.py
@@ -0,0 +1,8 @@
+"""LeapYear"""
+
+year = int(input("Enter year: "))
+
+if year % 4 == 0:
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
---
+++ histogram.py
@@ -0,0 +1,5 @@
+def histogram(number):
+    for n in number:
+        print("*" * n)
+        
+histogram([2,4,7])
