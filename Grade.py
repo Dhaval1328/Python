1e27631167e569fa6025451d54e8bda7e40c8dd8
Dhaval Parmar <ddhaval2655@gmail.com>
2026-02-17 10:48:41 +0530

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,14 @@
+'''Grade'''
+
+per = float(input("Enter percentage: "))
+
+if per >= 90:
+    print("Grade A")
+elif per >= 75:
+    print("Grade B")
+elif per >= 60:
+    print("Grade C")
+elif per >= 40:
+    print("Grade D")
+else:
+    print("Fail")
