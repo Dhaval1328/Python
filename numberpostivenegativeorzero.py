2a564b8fa917e5ad688c9f358349126bbb195a00
Dhaval Parmar <ddhaval2655@gmail.com>
2026-02-25 11:00:22 +0530

    Add files via upload

---
+++ numberpostivenegativeorzero.py
@@ -0,0 +1,11 @@
+"""WAP to check whether a number is Positive, Negative, or Zero"""
+
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Number is Positive",num)
+elif num < 0:
+    print("Number is Negative",num)
+else:
+    print("Number is Zero")
